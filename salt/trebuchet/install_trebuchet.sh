8cd85595a527d730507274bb25e80a4ee523ede5
Ariel T. Glenn <ariel@wikimedia.org>
2016-02-02 15:29:24 +0200

    make the trebuchet install script readable/maintainable
    move all the installs into separate functions, make a callable
    check function that can decide based on simple args whether
    an install needs to take place, check that salt ping to the
    deployment server works before starting the script (if it fails,
    none of the checks would have any meaning)

--- salt/trebuchet/install_trebuchet.sh
+++ salt/trebuchet/install_trebuchet.sh
@@ -1,26 +1,63 @@
 #!/bin/bash
 
-# for wmf testing purposes use a jessie host for the deployment server
+# Note: for wmf testing purposes use a jessie host for the deployment server
 
-# check that we are on the salt master
-is_master_running=`pgrep salt-master`
-if [ -z "$is_master_running" ]; then
-  echo "This script needs to be run on the salt master; please"
-  echo "be sure you are on the right host and that the"
-  echo "salt master is running."
-  exit 1
-fi
+check_exists(){
+    name=$1
+    type=$2
+    IP=$3
+    case $type in
+	'dir')
+	    command="ls -d $name"
+	    ;;
+	'file')
+	    command="ls $name"
+	    ;;
+	'pkg')
+	    command="dpkg -l | grep $name"
+	    ;;
+	*)
+	    echo "Unknown existence check $name $type"
+	    exit 1
+	    ;;
+    esac
+    command="$command 2>/dev/null"
+    result=`salt $IP cmd.run "$command" | grep -v $IP`
+    if [ ! -z "$result" ]; then
+	return 1
+    else
+	return 0
+    fi
+}    
 
-# check that we've been given the hostname of the redis/deploy server
-if [ -z "$1" ]; then
-  echo "Usage: $0 hostname"
-  echo "where hostname is the name of the minion to be used as the"
-  echo "development server and also the redis server."
-  exit 1
-fi
-SOMETHING="$1"
+check_running_on_master(){
+    # check that we are on the salt master
+    is_master_running=`pgrep salt-master`
+    if [ -z "$is_master_running" ]; then
+      echo "This script needs to be run on the salt master; please"
+      echo "be sure you are on the right host and that the"
+      echo "salt master is running."
+      exit 1
+    fi
+}
 
-if [ ! -e "/root/treb-tmp/trebuchet" ]; then
+check_args(){
+    # check that we've been given the hostname of the redis/deploy server
+    if [ -z "$1" ]; then
+      echo "Usage: $0 hostname"
+      echo "where hostname is the name of the minion to be used as the"
+      echo "development server and also the redis server."
+      exit 1
+    fi
+    DEPSERVER="$1"
+}
+
+check_depserver_responsive(){
+    host="$1"
+    salt "$host" test.ping | grep "$host" || (echo "$host not responsive to test.ping, exiting" ; return 1)
+}
+
+set_up_and_patch_trebuchet() {
   echo "setting up trebuchet on master"
 
   mkdir -p /root/treb-tmp
@@ -80,21 +117,19 @@ EOF
     cp modules/deploy.py /srv/salt/_modules/
     cd ..
   fi
-fi
+}
 
-ipadded=`grep $SOMETHING /etc/hosts`
-if [ -z "$ipadded" ]; then
+add_deployserver_ip_on_cluster(){
   echo "adding deploy server ip to /etc/hosts on all hosts"
 
-  output=`salt $SOMETHING cmd.run 'ip -4 addr show dev eth0 | grep inet'`
+  output=`salt $DEPSERVER cmd.run 'ip -4 addr show dev eth0 | grep inet'`
   IP=`echo $output | cut -f 3,3 -d ' ' | cut -f 1,1 -d /`
 
-  echo "$IP   $SOMETHING"  >> /etc/hosts
-  salt '*' cmd.run "echo $IP   $SOMETHING  >> /etc/hosts"
-fi
+  echo "$IP   $DEPSERVER"  >> /etc/hosts
+  salt '*' cmd.run "echo $IP   $DEPSERVER  >> /etc/hosts"
+}
 
-masterconf_done=`grep pillar_roots /etc/salt/master`
-if [ -z "$masterconf_done" ]; then
+add_trebuchet_opts_saltmaster(){
   echo "adding trebuchet options to saltmaster conf"
   # note that we add 'test' module commands to peer_run in case you need
   # to do basic debugging
@@ -104,40 +139,34 @@ pillar_roots:
     - /srv/pillars
 
 peer_run:
-  $SOMETHING:
+  $DEPSERVER:
     - deploy.*
     - test.*
 EOF
   ) >> /etc/salt/master
-fi
+}
 
-# install python redis bindings everywhere
-python_redis_installed=`salt $SOMETHING cmd.run 'dpkg -l | grep "python-redis" ' | grep -v ${SOMETHING}`
-if [ -z "$python_redis_installed" ]; then
+install_pyredis_bindings(){
   echo "installing python redis bindings everywhere"
   apt-get -y install python-redis
 
   # run on all minions
   salt -l debug '*' --timeout 60 cmd.run 'apt-get -y install python-redis'
-fi
+}
 
-# install redis on the right host and set up its config
-redis_installed=`salt $SOMETHING cmd.run 'dpkg -l | grep "redis-server" ' | grep -v ${SOMETHING}`
-if [ -z "$redis_installed" ]; then
+install_redis_server(){
   echo "installing/configuring redis server"
-  salt "$SOMETHING" --timeout 60 cmd.run "apt-get install -y redis-server"
-  salt "$SOMETHING" --timeout 60 cmd.run "cat /etc/redis/redis.conf | sed -e 's/^bind/#bind/g;' > /etc/redis/redis.conf.new"
-  salt "$SOMETHING" --timeout 60 cmd.run "mv /etc/redis/redis.conf.new /etc/redis/redis.conf"
-  salt "$SOMETHING" --timeout 60 cmd.run "/etc/init.d/redis-server start"
-  salt "$SOMETHING" --timeout 60 cmd.run "apt-get -y install sudo"
-fi
+  salt "$DEPSERVER" --timeout 60 cmd.run "apt-get install -y redis-server"
+  salt "$DEPSERVER" --timeout 60 cmd.run "cat /etc/redis/redis.conf | sed -e 's/^bind/#bind/g;' > /etc/redis/redis.conf.new"
+  salt "$DEPSERVER" --timeout 60 cmd.run "mv /etc/redis/redis.conf.new /etc/redis/redis.conf"
+  salt "$DEPSERVER" --timeout 60 cmd.run "/etc/init.d/redis-server start"
+  salt "$DEPSERVER" --timeout 60 cmd.run "apt-get -y install sudo"
+}
 
-# clone and patch trigger
-trigger_cloned=`salt $SOMETHING cmd.run 'ls -d /root/trigger-tmp/trigger 2>/dev/null' | grep -v ${SOMETHING}`
-if [ -z "$trigger_cloned" ]; then
+clone_patch_trigger(){
   echo "cloning trigger"
-  salt "$SOMETHING" cmd.run "mkdir -p /root/trigger-tmp"
-  salt "$SOMETHING" cmd.run "cd /root/trigger-tmp; git clone https://github.com/trebuchet-deploy/trigger.git"
+  salt "$DEPSERVER" cmd.run "mkdir -p /root/trigger-tmp"
+  salt "$DEPSERVER" cmd.run "cd /root/trigger-tmp; git clone https://github.com/trebuchet-deploy/trigger.git"
   ( cat <<'EOF'
 --- a/trigger/drivers/trebuchet/local.py
 +++ b/trigger/drivers/trebuchet/local.py
@@ -191,28 +220,25 @@ if [ -z "$trigger_cloned" ]; then
          ## Disabled until salt bug is fixed:
 EOF
   ) > /root/local.py.patch
-  patchdone=`salt "$SOMETHING" cmd.run 'grep saltstack/salt/issues/18317 /root/trigger-tmp/trigger/drivers/trebuchet/local.py'`
+  patchdone=`salt "$DEPSERVER" cmd.run 'grep saltstack/salt/issues/18317 /root/trigger-tmp/trigger/drivers/trebuchet/local.py'`
   if [ -z "$patchdone" ]; then
       echo "patching trigger"
-      salt-cp "$SOMETHING" /root/local.py.patch /root/local.py.patch
-      salt "$SOMETHING" cmd.run '(cd /root/trigger-tmp/trigger/trigger/drivers/trebuchet/; patch < /root/local.py.patch)'
+      salt-cp "$DEPSERVER" /root/local.py.patch /root/local.py.patch
+      salt "$DEPSERVER" cmd.run '(cd /root/trigger-tmp/trigger/trigger/drivers/trebuchet/; patch < /root/local.py.patch)'
   fi
-fi
+}
 
-
-python_git_installed=`salt $SOMETHING cmd.run "dpkg -l | grep 'python-git' " | grep -v ${SOMETHING}`
-if [ -z "${python_git_installed}" ]; then
+install_pygit(){
   echo "installing python-git and depenencies"
-  salt "$SOMETHING" cmd.run 'apt-get -y install python-setuptools'
+  salt "$DEPSERVER" cmd.run 'apt-get -y install python-setuptools'
 
   # ok on trusty and jessie but not precise, steal a backport.
   command='grep 12.04 /etc/issue; if [ $? -eq 1 ]; then apt-get -y install python-git; else apt-get install git-core; apt-get -y install curl; curl -o "/root/python-git_0.3.2.RC1-1_all.deb" "http://apt.wikimedia.org/wikimedia/pool/universe/p/python-git/python-git_0.3.2.RC1-1_all.deb"; curl -o "/root/python-async_0.6.1-1~precise1_amd64.deb" "http://apt.wikimedia.org/wikimedia/pool/universe/p/python-async/python-async_0.6.1-1~precise1_amd64.deb"; curl -o "/root/python-gitdb_0.5.4-1~precise1_amd64.deb" "http://apt.wikimedia.org/wikimedia/pool/universe/p/python-gitdb/python-gitdb_0.5.4-1~precise1_amd64.deb"; curl -o "/root/python-smmap_0.8.2-1~precise1_all.deb" "http://apt.wikimedia.org/wikimedia/pool/universe/p/python-smmap/python-smmap_0.8.2-1~precise1_all.deb"; dpkg -i "/root/python-smmap_0.8.2-1~precise1_all.deb"; dpkg -i "/root/python-async_0.6.1-1~precise1_amd64.deb"; dpkg -i "/root/python-gitdb_0.5.4-1~precise1_amd64.deb"; dpkg -i "/root/python-git_0.3.2.RC1-1_all.deb"; fi'
   echo "command is" $command
-  salt "$SOMETHING" --timeout 60 cmd.run "$command"
-fi
+  salt "$DEPSERVER" --timeout 60 cmd.run "$command"
+}
 
-# put config files in place
-if [ ! -e /srv/pillars/top.sls ]; then
+setup_pillars(){
   echo "setting up pillars"
   mkdir -p /srv/pillars
   ( cat <<'EOF'
@@ -227,92 +253,86 @@ base:
     - deployment.deployment_config
 EOF
   ) > /srv/pillars/top.sls
-fi
+}
 
-if [ ! -e /srv/pillars/deployment/deployment_config.sls ]; then
+setup_more_pillars(){
   echo "setting up more pillars"
   mkdir -p /srv/pillars/deployment
  ( cat <<EOF
-{"deployment_config": {"parent_dir": "/srv/deployment", "redis": {"db": 0, "host": "$SOMETHING", "port": 6379}, "servers": {"default": "$SOMETHING"}}}
+{"deployment_config": {"parent_dir": "/srv/deployment", "redis": {"db": 0, "host": "$DEPSERVER", "port": 6379}, "servers": {"default": "$DEPSERVER"}}}
 EOF
   ) > /srv/pillars/deployment/deployment_config.sls
   ( cat <<'EOF'
   {"repo_config": {"test/testrepo": {"checkout_submodules": true, "service_name": "puppet"}}}
 EOF
   ) > /srv/pillars/deployment/repo_config.sls
-fi
+}
 
-# set grains on minions
-echo "setting grains on minions"
-salt '*' grains.setval 'deployment_target' 'test/testrepo'
-salt '*' grains.setval 'trebuchet_master' "$SOMETHING"
-salt '*' grains.setval 'site' "default"
-# don't really want the deployment server to be a deployment target I guess
-salt "$SOMETHING" grains.delval 'deployment_target' destructive=True
-salt "$SOMETHING" grains.setval 'deployment_server' 'true'
-salt "$SOMETHING" grains.setval 'deployment_repo_user' 'root'
+setup_grains(){
+  # set grains on minions
+  echo "setting grains on minions"
+  salt '*' grains.setval 'deployment_target' 'test/testrepo'
+  salt '*' grains.setval 'trebuchet_master' "$DEPSERVER"
+  salt '*' grains.setval 'site' "default"
+  # don't really want the deployment server to be a deployment target I guess
+  salt "$DEPSERVER" grains.delval 'deployment_target' destructive=True
+  salt "$DEPSERVER" grains.setval 'deployment_server' 'true'
+  salt "$DEPSERVER" grains.setval 'deployment_repo_user' 'root'
+}
 
-# set up deploy_runner.conf on master, some versions of trebuchet need it
-if [ ! -e "/etc/salt/deploy_runner.conf" ]; then
+setup_deploy_runner_conf(){
+  # set up deploy_runner.conf on master, some versions of trebuchet need it
   echo "setting up deploy_runner.conf"
   ( cat <<'EOF'
 deployment_repo_grains: {'test/testrepo': 'test/testrepo',}
 EOF
   ) > /etc/salt/deploy_runner.conf
-fi
+}
 
-# push out module to minions
-echo "refreshing modules out to minions"
-/etc/init.d/salt-master restart
+refresh_modules(){
+  echo "refreshing modules out to minions"
+  /etc/init.d/salt-master restart
 
-# let master wake up
-sleep 15
-salt '*' test.ping
+  # let master wake up
+  sleep 15
+  salt '*' test.ping
 
-salt '*' saltutil.refresh_modules
-salt '*' saltutil.sync_all
-salt '*' cmd.run '/etc/init.d/salt-minion restart'
-# wait for minions to wake up
-sleep 5
-salt '*' test.ping
+  salt '*' saltutil.refresh_modules
+  salt '*' saltutil.sync_all
+  salt '*' cmd.run '/etc/init.d/salt-minion restart'
+  # wait for minions to wake up
+  sleep 5
+  salt '*' test.ping
+}
 
-echo "configuring repo for git deploy"
-salt "$SOMETHING" deploy.deployment_server_init
-
-# make test repo on deployment server
-gitconfig_exists=`salt $SOMETHING cmd.run 'ls /root/.gitconfig 2>/dev/null' | grep -v ${SOMETHING}`
-if [ -z "$gitconfig_exists" ]; then
+setup_gitconfig(){
   echo "setting up git config for root"
   # fix the 'stdin: not a tty' error
-  salt "$SOMETHING" cmd.run 'grep -v mesg /root/.profile > /root/.profile.new; mv /root/.profile.new /root/.profile'
+  salt "$DEPSERVER" cmd.run 'grep -v mesg /root/.profile > /root/.profile.new; mv /root/.profile.new /root/.profile'
   # set up the config
-  salt "$SOMETHING" cmd.run "bash -c 'HOME=/root git config --global user.name \"A. Tester\"'"
-  salt "$SOMETHING" cmd.run "bash -c 'HOME=/root git config --global user.email atester@noreply.com'"
-fi
+  salt "$DEPSERVER" cmd.run "bash -c 'HOME=/root git config --global user.name \"A. Tester\"'"
+  salt "$DEPSERVER" cmd.run "bash -c 'HOME=/root git config --global user.email atester@noreply.com'"
+}
 
-testrepo_exists=`salt $SOMETHING cmd.run 'ls /srv/deployment/test/testrepo/myfile.txt 2>/dev/null' | grep -v ${SOMETHING}`
-if [ -z $testrepo_exists ]; then
+setup_test_repo(){
   echo "setting up initial test repo"
-  salt "$SOMETHING" cmd.run "mkdir -p /srv/deployment/test/testrepo"
-  salt "$SOMETHING" cmd.run "cd /srv/deployment/test/testrepo; git init"
-  salt "$SOMETHING" cmd.run 'echo "bla bla" > /srv/deployment/test/testrepo/myfile.txt'
-  salt "$SOMETHING" cmd.run 'git --git-dir=/srv/deployment/test/testrepo/.git --work-tree=/srv/deployment/test/testrepo add myfile.txt'
-  salt "$SOMETHING" cmd.run "bash -c 'HOME=/root git --git-dir=/srv/deployment/test/testrepo/.git --work-tree=/srv/deployment/test/testrepo commit -m junk_for_testing'"
-fi
+  salt "$DEPSERVER" cmd.run "mkdir -p /srv/deployment/test/testrepo"
+  salt "$DEPSERVER" cmd.run "cd /srv/deployment/test/testrepo; git init"
+  salt "$DEPSERVER" cmd.run 'echo "bla bla" > /srv/deployment/test/testrepo/myfile.txt'
+  salt "$DEPSERVER" cmd.run 'git --git-dir=/srv/deployment/test/testrepo/.git --work-tree=/srv/deployment/test/testrepo add myfile.txt'
+  salt "$DEPSERVER" cmd.run "bash -c 'HOME=/root git --git-dir=/srv/deployment/test/testrepo/.git --work-tree=/srv/deployment/test/testrepo commit -m junk_for_testing'"
+}
 
-# set up apache on deployment server
-apache_installed=`salt $SOMETHING cmd.run 'dpkg -l | grep apache2' | grep -v ${SOMETHING}`
-if [ -z "$apache_installed" ]; then
-  echo "installing apache"
-  salt "$SOMETHING" cmd.run "apt-get -y install apache2"
-fi
+install_apache(){
+  echo "installing apache on deployment server"
+  salt "$DEPSERVER" cmd.run "apt-get -y install apache2"
+}
 
-apache_conf_done=`salt $SOMETHING cmd.run 'ls /etc/apache2/sites-available/deployment.conf 2>/dev/null' | grep -v ${SOMETHING}`
-if [ -z "$apache_conf_done" ]; then
+setup_apache_config(){
   echo "setting up apache config"
   ( cat <<EOF
 <VirtualHost *:80>
- ServerName $SOMETHING
+ ServerName $DEPSERVER
  ServerAdmin atester@noreply.com
  DocumentRoot /srv/deployment
 
@@ -331,12 +351,12 @@ if [ -z "$apache_conf_done" ]; then
 EOF
   ) >> /root/apache_deployment_conf
 
-  salt-cp "$SOMETHING" /root/apache_deployment_conf /root/apache_deployment_conf
-  salt "$SOMETHING" cmd.run "cp /root/apache_deployment_conf /etc/apache2/sites-available/deployment.conf"
-  salt "$SOMETHING" cmd.run "cd /etc/apache2/sites-enabled/; ln -s ../sites-available/deployment.conf ."
-  salt "$SOMETHING" cmd.run "rm /etc/apache2/sites-enabled/000-default"
+  salt-cp "$DEPSERVER" /root/apache_deployment_conf /root/apache_deployment_conf
+  salt "$DEPSERVER" cmd.run "cp /root/apache_deployment_conf /etc/apache2/sites-available/deployment.conf"
+  salt "$DEPSERVER" cmd.run "cd /etc/apache2/sites-enabled/; ln -s ../sites-available/deployment.conf ."
+  salt "$DEPSERVER" cmd.run "rm /etc/apache2/sites-enabled/000-default"
 
-  main_conf_fixed=`salt $SOMETHING cmd.run 'egrep "^<Directory /srv" /etc/apache2/apache2.conf 2>/dev/null' | grep srv`
+  main_conf_fixed=`salt $DEPSERVER cmd.run 'egrep "^<Directory /srv" /etc/apache2/apache2.conf 2>/dev/null' | grep srv`
   if [ -z "$main_conf_fixed" ]; then
   ( cat <<'EOF'
 <Directory /srv/>
@@ -348,23 +368,48 @@ EOF
 </Directory>
 EOF
   ) > /root/apache_additions
-    salt-cp "$SOMETHING" /root/apache_additions /root/apache_additions
-    salt "$SOMETHING" cmd.run "cat /root/apache_additions >> /etc/apache2/apache2.conf"
+    salt-cp "$DEPSERVER" /root/apache_additions /root/apache_additions
+    salt "$DEPSERVER" cmd.run "cat /root/apache_additions >> /etc/apache2/apache2.conf"
   fi
-fi
+}
 
-# (re) start the services we need
-salt "$SOMETHING" --timeout 60 cmd.run "/etc/init.d/apache2 restart"
-salt "$SOMETHING" --timeout 60 cmd.run "/etc/init.d/redis-server restart"
+restart_apache_redis(){
+  # (re) start the services we need
+  salt "$DEPSERVER" --timeout 60 cmd.run "/etc/init.d/apache2 restart"
+  salt "$DEPSERVER" --timeout 60 cmd.run "/etc/init.d/redis-server restart"
+}
 
-# install trigger to the right place
-trigger_installed=`salt $SOMETHING cmd.run 'ls -d /usr/share/pyshared/trigger 2>/dev/null | grep "trigger" ' | grep -v ${SOMETHING}`
-if [ -z "$trigger_installed" ]; then
-  echo "installing trigger"
-  salt "$SOMETHING" cmd.run '(cd /root/trigger-tmp/trigger; python setup.py install)'
-fi
+install_trigger(){
+  echo "installing trigger on deployment server"
+  salt "$DEPSERVER" cmd.run '(cd /root/trigger-tmp/trigger; python setup.py install)'
+}
+
+#########
+
+check_running_on_master
+check_args "$1"
+check_depserver_responsive $DEPSERVER || exit 1
+[ ! -e "/root/treb-tmp/trebuchet" ] && set_up_and_patch_trebuchet
+grep $DEPSERVER /etc/hosts  > /dev/null || add_deployserver_ip_on_cluster
+grep pillar_roots /etc/salt/master > /dev/null || add_trebuchet_opts_saltmaster
+check_exists "python-redis" "pkg" "$DEPSERVER" && install_pyredis_bindings
+check_exists "redis-server" "pkg" "$DEPSERVER" && install_redis_server
+check_exists "/root/trigger-tmp/trigger" "dir" "$DEPSERVER" && clone_patch_trigger
+check_exists "python-git" "pkg" "$DEPSERVER" && install_pygit
+[ ! -e /srv/pillars/top.sls ] && setup_pillars
+[ ! -e /srv/pillars/deployment/deployment_config.sls ] && setup_more_pillars
+setup_grains
+[ ! -e "/etc/salt/deploy_runner.conf" ] && setup_deploy_runner_conf
+refresh_modules
+echo "configuring repo for git deploy"; salt "$DEPSERVER" deploy.deployment_server_init
+check_exists "/root/.gitconfig" "file" "$DEPSERVER" && setup_gitconfig
+check_exists "/srv/deployment/test/testrepo/myfile.txt" "file" "$DEPSERVER" && setup_test_repo
+check_exists "apache2" "pkg" "$DEPSERVER" && install_apache
+check_exists "/etc/apache2/sites-available/deployment.conf" "file" "$DEPSERVER" && setup_apache_config
+restart_apache_redis
+check_exists "/usr/share/pyshared/trigger" "dir" "$DEPSERVER" && install_trigger
 
 echo "done!"
 
 # fixmes: might be nice not to set the grains if already set
-#         also maybe not re-export the modules etc if they are already showing up on minions
+# also maybe not re-export the modules etc if they are already showing up on minions
